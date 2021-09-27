c9b9da660b2bba90a7b53f9c0c28f3b2afa7537f
hakanakgunes <hakanakgunes87@gmail.com>
2021-09-27 21:33:26 +0300

    initial commit

---
+++ src/main/java/com/example/coinalert/CoinAlertApplication.java
@@ -0,0 +1,13 @@
+package com.example.coinalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinAlertApplication.class, args);
+    }
+
+}
